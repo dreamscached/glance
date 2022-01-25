74bd27a577f1052f4b6fa774c3eb71ad2af68651
dreamscached <dreamscache.d@gmail.com>
2022-01-25 09:34:55 +0100

    Add default lang

--- glance/ocr/scan.py
+++ glance/ocr/scan.py
@@ -5,6 +5,10 @@ from PIL import Image
 def scan(path, args):
     with open(path, "rb") as fp:
         im = Image.open(fp)
-        data = pytesseract.image_to_string(im, str.join("+", args.lang))
+        if args.lang is None:
+            lang = "eng"
+        else:
+            lang = str.join("+", args.lang)
+        data = pytesseract.image_to_string(im, lang)
 
     return data
